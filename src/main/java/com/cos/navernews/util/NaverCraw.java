db9a1c54d19793c39246c1f22c20d5438867d38b
saram <saram9p@gmail.com>
2021-09-28 17:09:46 +0900

    NaverCraw.java 예외처리

--- src/main/java/com/cos/navernews/util/NaverCraw.java
+++ src/main/java/com/cos/navernews/util/NaverCraw.java
@@ -25,23 +25,28 @@ public class NaverCraw {
 			String url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=102&oid=022&aid=" + aid;
 			String html = rt.getForObject(url, String.class);
 			
-			Document doc = Jsoup.parse(html);
-			
-			Element companyElement    = doc.selectFirst(".press_logo img ");
-			Element titleElement            = doc.selectFirst("#articleTitle");
-			Element createdAtElement = doc.selectFirst(".sponsor .t11");
-			
-			String company = companyElement.attr("title");
-			String title = titleElement.text();
-			String createdAt = createdAtElement.text();
-			
-			News news = News.builder()
-					.company(company)
-					.title(title)
-					.createdAt(createdAt)
-					.build();
-			
-			newsList.add(news);
+			try {
+				Document doc = Jsoup.parse(html);
+				
+				Element companyElement    = doc.selectFirst(".press_logo img ");
+				Element titleElement            = doc.selectFirst("#articleTitle");
+				Element createdAtElement = doc.selectFirst(".sponsor .t11");
+				
+				String company = companyElement.attr("title");
+				String title = titleElement.text();
+				String createdAt = createdAtElement.text();
+				
+				News news = News.builder()
+						.company(company)
+						.title(title)
+						.createdAt(createdAt)
+						.build();
+				
+				newsList.add(news);
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("찾을수 없어요: " + e.getMessage());
+			}
 			
 			aidnum++;
 		}
